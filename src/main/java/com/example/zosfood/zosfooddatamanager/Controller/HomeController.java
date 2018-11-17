3718a47e1243cf87e6e7765d37ad4580e84ed9ca
Joselon <joselon1999@hotmail.com>
2018-11-17 01:25:51 -0500

    Funciona

--- src/main/java/com/example/zosfood/zosfooddatamanager/Controller/HomeController.java
+++ src/main/java/com/example/zosfood/zosfooddatamanager/Controller/HomeController.java
@@ -27,15 +27,15 @@ public class HomeController {
     @GetMapping("/fill")
     public String setValues(){
         categoryService.save(new Category(1,"Jojo's Part 1","Phantom Blood"));
-        usuarioService.save(new Usuario(1,"Jhonatan Joestar",new Stand(1,"Goodest Boi","Being Best JOJO")));
         categoryService.save(new Category(2,"Jojo's Part 2","Battle Tendency"));
-        usuarioService.save(new Usuario(1,"Jhoseph Joestar",new Stand(1,"Hermit Purple","Crash planes and NIGERUNDAYO")));
         categoryService.save(new Category(3,"Jojo's Part 3","Stardust Crusaders"));
-        usuarioService.save(new Usuario(1,"Jotaro Kuyo",new Stand(1,"Star Platinum","Fucks dolphins and can stop time sometimes")));
         categoryService.save(new Category(4,"Jojo's Part 4","Diamond is Unbreakable"));
-        usuarioService.save(new Usuario(1,"Josuke Higashikata",new Stand(1,"Crazy Diamond","As useless as Joseph but GRATO DAZE")));
         categoryService.save(new Category(5,"Jojo's Part 5","Vento Aureo"));
-        usuarioService.save(new Usuario(1,"Giorno Giovanna",new Stand(1,"Gold Experience","Brings shit to life and drinks piss")));
+        usuarioService.save(new Usuario(1,"Jhonatan Joestar",new Stand(1,"Goodest Boi","Being Best JOJO")));
+        usuarioService.save(new Usuario(2,"Jhoseph Joestar",new Stand(1,"Hermit Purple","Crash planes and NIGERUNDAYO")));
+        usuarioService.save(new Usuario(3,"Jotaro Kuyo",new Stand(1,"Star Platinum","Fucks dolphins and can stop time sometimes")));
+        usuarioService.save(new Usuario(4,"Josuke Higashikata",new Stand(1,"Crazy Diamond","As useless as Joseph but GRATO DAZE")));
+        usuarioService.save(new Usuario(5,"Giorno Giovanna",new Stand(1,"Gold Experience","Brings shit to life and drinks piss")));
         return "Listo Papuh";
     }
 }
